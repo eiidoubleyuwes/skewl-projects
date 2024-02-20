e565e402d1b3d963d91a8dd79b5117dbea0d2e33
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-20 11:59:16 +0300

    Feat:Added Client side code
    Learning socket programming

--- localip.java
+++ localip.java
@@ -1,3 +1,17 @@
 //This script is supposed to get the local Ip address
 
-import java.net.Ip
+File: MyClient.java
+import java.io.*;  
+import java.net.*;  
+public class MyClient {  
+public static void main(String[] args) {  
+try{      
+Socket s=new Socket("localhost",6666);  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+dout.writeUTF("Hello Server");  
+dout.flush();  
+dout.close();  
+s.close();  
+}catch(Exception e){System.out.println(e);}  
+}  
+} 
